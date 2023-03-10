f2ead0fca33e3027f9f7efdd5427a4db794b40bb
Matias Arias <matiasarias59@gmail.com>
2023-03-10 18:14:45 -0300

    Add background home page

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <div className="HomeScreen">
+      <div className="HomeScreen__circle">
+      <div className="HomeScreen__circle__inside"></div>
+
+      </div>
+      <div className="HomeScreen__bg">
+        <div className="HomeScreen__bg__red"></div>
+        <div className="HomeScreen__bg__black"></div>
+        <div className="HomeScreen__bg__white"></div>
+      </div>
+    </div>
+  );
+}
